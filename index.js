fb6d66eac6ad1312aa35cdb3199bc4fc6e596b91
royoswaldha <88558831+royoswaldha@users.noreply.github.com>
2024-02-13 16:49:12 +0700

    Ta
    ta

---
+++ index.js
@@ -0,0 +1,22 @@
+// index.js
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Middleware untuk menyajikan file statis
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'src')));
+
+// Route ke index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/src/index.html'));
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan pada http://localhost:${PORT}`);
+});
