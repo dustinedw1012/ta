64f54c1e6c5ff708e45eed0e5b0854ab8dcc11e7
dustinedw1012 <132728512+dustinedw1012@users.noreply.github.com>
2024-05-26 02:41:55 +0700

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+cd /opt/bitnami/projects/ta
+
+pm2 start index.js --name sato-jabar
+
+# Save the PM2 process list
+pm2 save
+
+# Generate PM2 startup script
+pm2 startup systemd -u bitnami --hp /opt/bitnami
+
+# Set PM2 to start on boot
+sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u bitnami --hp /opt/bitnami
+
 # Start the PM2 service
 sudo systemctl start bitnami.service
 
