05f641628327d3bd86b65045540097b21c696dce
dustinedw1012 <132728512+dustinedw1012@users.noreply.github.com>
2024-05-26 02:25:35 +0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,6 +2,7 @@
 
 echo "Preparing for installation..."
 
+find /opt/bitnami/projects/ta/ -name '.DS_Store' -type f -delete
 sudo systemctl stop bitnami.service
 
 echo "before_install.sh script completed."
