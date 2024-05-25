773671f5f484b31bde753b1b0bde5a764d8b6327
dustinedw1012 <132728512+dustinedw1012@users.noreply.github.com>
2024-05-26 02:21:24 +0700

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the PM2 service
-sudo systemctl start pm2-bitnami.service
+sudo systemctl start bitnami.service
 
 # Reload PM2 process list and start the application
 pm2 reload all
