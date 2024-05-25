9707e09261f930995ec60b82977a65bb00b47b64
dustinedw1012 <132728512+dustinedw1012@users.noreply.github.com>
2024-05-26 02:40:13 +0700

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,6 +3,8 @@
 echo "Preparing for installation..."
 
 sudo systemctl stop bitnami.service
+shopt -s dotglob
 sudo rm -rf /opt/bitnami/projects/ta/*
+sudo rm -rf /opt/bitnami/projects/ta/.* || true
 
 echo "before_install.sh script completed."
